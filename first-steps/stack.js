de90a263e88fcba1f8b6377d723f36a4ac92bcfe
Golo Roden <golo.roden@thenativeweb.io>
2017-03-18 14:56:46 +0100

    Add stack.

--- first-steps/stack.js
+++ first-steps/stack.js
@@ -1,7 +1,39 @@
-
+'use strict';
 
 const createStack = function () {
-  // ...
+  const items = [];
+  let itemsCount = 0;
+
+  const stack = {
+    isEmpty () {
+      return itemsCount === 0;
+    },
+
+    push (value) {
+      items[itemsCount] = value;
+      itemsCount++;
+    },
+
+    top () {
+      if (stack.isEmpty()) {
+        throw new Error('Stack is empty.');
+      }
+
+      return items[itemsCount - 1];
+    },
+
+    pop () {
+      if (stack.isEmpty()) {
+        throw new Error('Stack is empty.');
+      }
+
+      itemsCount--;
+
+      return items[itemsCount];
+    }
+  };
+
+  return stack;
 };
 
 const stack = createStack();
